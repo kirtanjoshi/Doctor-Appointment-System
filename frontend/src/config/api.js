d7c420bcab17eb9bd0e77515d9efb2b803a9dd21
kirtanjoshi <kirtikirtanj@gmail.com>
2025-08-30 15:21:50 +0545

    Bug Fixes

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api';
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api';
+const API_BASE_URL = 'https://doctor-appointment-system-1tpn.onrender.com/api' ;
 export default API_BASE_URL;
